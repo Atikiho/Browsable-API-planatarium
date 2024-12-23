8f20f7b8e5b45cf11d616c6bddc184d260f08fcd
user <nikitaskrypets@gmail.com>
2024-12-23 12:37:54 +0200

    created views

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from planetarium.models import (
+    ShowTheme,
+    AstronomyShow,
+    PlanetariumDome,
+    ShowSession,
+    Reservation,
+    Ticket
+)
+from planetarium.serializers import (
+    ShowThemeSerializer,
+    AstronomyShowSerializer,
+    PlanetariumDomeSerializer,
+    ShowSessionSerializer,
+    ReservationSerializer,
+    TicketSerializer,
+    ShowSessionDetailSerializer
+)
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    serializer_class = ShowThemeSerializer
+    queryset = ShowTheme.objects.all()
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    serializer_class = AstronomyShowSerializer
+    queryset = AstronomyShow.objects.all()
+
+
+class PlanetariumDomeViewSet(viewsets.ModelViewSet):
+    serializer_class = PlanetariumDomeSerializer
+    queryset = PlanetariumDome.objects.all()
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    serializer_class = ShowSessionSerializer
+    queryset = ShowSession.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return ShowSessionSerializer
+        elif self.action == "retrieve":
+            return ShowSessionDetailSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    serializer_class = ReservationSerializer
+    queryset = Reservation.objects.all()
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    serializer_class = TicketSerializer
+    queryset = Ticket.objects.all()
