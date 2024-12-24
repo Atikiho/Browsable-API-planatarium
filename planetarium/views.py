f6e1b021ed0c76fbbf54e303bf3e0f44cc24ea0b
user <nikitaskrypets@gmail.com>
2024-12-24 14:19:24 +0200

    add permissions

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from planetarium.models import (
     ShowTheme,
@@ -8,6 +9,7 @@ from planetarium.models import (
     Reservation,
     Ticket
 )
+from planetarium.permissions import IsAdminOrIfAuthenticatedReadOnly
 from planetarium.serializers import (
     ShowThemeSerializer,
     AstronomyShowSerializer,
@@ -22,34 +24,40 @@ from planetarium.serializers import (
 class ShowThemeViewSet(viewsets.ModelViewSet):
     serializer_class = ShowThemeSerializer
     queryset = ShowTheme.objects.all()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class AstronomyShowViewSet(viewsets.ModelViewSet):
     serializer_class = AstronomyShowSerializer
     queryset = AstronomyShow.objects.all()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class PlanetariumDomeViewSet(viewsets.ModelViewSet):
     serializer_class = PlanetariumDomeSerializer
     queryset = PlanetariumDome.objects.all()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
 
 class ShowSessionViewSet(viewsets.ModelViewSet):
-    serializer_class = ShowSessionSerializer
     queryset = ShowSession.objects.all()
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
 
     def get_serializer_class(self):
         if self.action == "list":
             return ShowSessionSerializer
         elif self.action == "retrieve":
             return ShowSessionDetailSerializer
+        return ShowSessionSerializer
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
     serializer_class = ReservationSerializer
     queryset = Reservation.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class TicketViewSet(viewsets.ModelViewSet):
     serializer_class = TicketSerializer
     queryset = Ticket.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
