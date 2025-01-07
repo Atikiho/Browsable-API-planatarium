e33cb2ed401c109f9b4f4d0a1c930f93d87de321
user <nikitaskrypets@gmail.com>
2025-01-07 17:28:07 +0200

    made secret key dynamic

--- core/settings.py
+++ core/settings.py
@@ -13,15 +13,18 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-j7qajmp)vewil394$5&b8fi7m&f#(802gh5$835s9!kt5nc!#y"
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-j7qajmp)vewil394$5&b8fi7m&f#(802gh5$835s9!kt5nc!#y")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
